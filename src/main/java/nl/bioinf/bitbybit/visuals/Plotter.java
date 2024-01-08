08ed69a7b6cd945b2fdbd70009c8f6f3a7eff181
Muniru <mawal-umaru@st.hanze.nl>
2024-01-08 12:06:58 +0100

    Added: Plotter.java

---
+++ src/main/java/nl/bioinf/bitbybit/visuals/Plotter.java
@@ -0,0 +1,21 @@
+package nl.bioinf.bitbybit.visuals;
+
+import nl.bioinf.bitbybit.data.WatchData;
+import nl.bioinf.bitbybit.data.WatchType;
+import
+
+public class Plotter {
+
+    private WatchData watchData;
+
+    public Plotter(WatchData watchData){
+        this.watchData = watchData;
+    }
+
+    public Object StepVisual(int unixFrom, int unixTo){
+
+
+        //https://www.highcharts.com/demo/highcharts/column-rotated-labels
+        return null;
+    }
+}
