188b4317db5c74a63a00ead45bf831552245c121
ltennapel <l.ten.napel@st.hanze.nl>
2024-01-10 12:29:46 +0100

    I created the web.xml, optimised some code to fit the 70 char mark and made the upload function work

--- src/main/java/nl/bioinf/bitbybit/file/TestZipReader.java
+++ src/main/java/nl/bioinf/bitbybit/file/TestZipReader.java
@@ -1,35 +1,35 @@
-package nl.bioinf.bitbybit.file;
-
-import java.nio.file.*;
-import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-public class TestZipReader {
-    public static void main(String[] args) {
-        String zipFilePath = "path/to/your/file.zip";
-        String tempDirectoryPath = "path/to/temp/directory";
-
-        try {
-            unzip(zipFilePath, tempDirectoryPath);
-            System.out.println("Zip file extracted successfully to: " + tempDirectoryPath);
-        } catch (IOException e) {
-            System.err.println("Error extracting zip file: " + e.getMessage());
-        }
-    }
-
-    private static void unzip(String zipFilePath, String destDirectory) throws IOException {
-        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath))) {
-            ZipEntry entry;
-            while ((entry = zipInputStream.getNextEntry()) != null) {
-                Path entryPath = Paths.get(destDirectory, entry.getName());
-                if (entry.isDirectory()) {
-                    Files.createDirectories(entryPath);
-                } else {
-                    Files.copy(zipInputStream, entryPath, StandardCopyOption.REPLACE_EXISTING);
-                }
-                zipInputStream.closeEntry();
-            }
-        }
-    }
-}
+//package nl.bioinf.bitbybit.file;
+//
+//import java.nio.file.*;
+//import java.io.IOException;
+//import java.util.zip.ZipEntry;
+//import java.util.zip.ZipInputStream;
+//
+//public class TestZipReader {
+//    public static void main(String[] args) {
+//        String zipFilePath = "path/to/your/file.zip";
+//        String tempDirectoryPath = "path/to/temp/directory";
+//
+//        try {
+//            unzip(zipFilePath, tempDirectoryPath);
+//            System.out.println("Zip file extracted successfully to: " + tempDirectoryPath);
+//        } catch (IOException e) {
+//            System.err.println("Error extracting zip file: " + e.getMessage());
+//        }
+//    }
+//
+//    private static void unzip(String zipFilePath, String destDirectory) throws IOException {
+//        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath))) {
+//            ZipEntry entry;
+//            while ((entry = zipInputStream.getNextEntry()) != null) {
+//                Path entryPath = Paths.get(destDirectory, entry.getName());
+//                if (entry.isDirectory()) {
+//                    Files.createDirectories(entryPath);
+//                } else {
+//                    Files.copy(zipInputStream, entryPath, StandardCopyOption.REPLACE_EXISTING);
+//                }
+//                zipInputStream.closeEntry();
+//            }
+//        }
+//    }
+//}
--- src/main/java/nl/bioinf/bitbybit/servlets/FilePageServlet.java
+++ src/main/java/nl/bioinf/bitbybit/servlets/FilePageServlet.java
@@ -1,65 +1,63 @@
 package nl.bioinf.bitbybit.servlets;
-import nl.bioinf.bitbybit.config.WebConfig; //change to your situation!
-import nl.bioinf.bitbybit.file.FileHandler;
+
+import nl.bioinf.bitbybit.config.WebConfig;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Part;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Locale;
 
 @WebServlet(name = "FilePageServlet", urlPatterns = "/file-upload")
 public class FilePageServlet extends HttpServlet {
+
     private TemplateEngine templateEngine;
+    private String uploadDir;
 
     @Override
     public void init() throws ServletException {
-        System.out.println("Initializing Thymeleaf template engine");
         final ServletContext servletContext = this.getServletContext();
         this.templateEngine = WebConfig.createTemplateEngine(servletContext);
+        this.uploadDir = servletContext.getInitParameter("upload_dir");
+        System.out.println(uploadDir);
+
+        //or, use relative to this app:
+        // gets absolute path of the web application
+        //String applicationPath = getServletContext().getRealPath("");
+        //String uploadFilePath = applicationPath + File.separator + uploadDir;
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String watchType = request.getParameter("watch_category");
-        Locale locale = request.getLocale();
-        WebContext ctx = new WebContext(
-                request,
-                response,
-                request.getServletContext(),
-                locale);
-        if (watchType != null) {
-            final String watch = FileHandler.getWatchType(watchType);
-            ctx.setVariable("watch_type", watchType);
-            ctx.setVariable("watch_msg", watch);
-            ctx.setVariable("file_name", request.getParameter("file_form"));
-        } else {
-            ctx.setVariable("watch_type", "none");
-            ctx.setVariable("watch_msg", "no msg");
-            ctx.setVariable("file_name", "no file");
-        }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //simply go back to upload form
+        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale());
         templateEngine.process("file-upload", ctx, response.getWriter());
     }
 
-    public void process(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        //this step is optional; standard settings also suffice
-        WebConfig.configureResponse(response);
-        WebContext ctx = new WebContext(
-                request,
-                response,
-                request.getServletContext(),
-                request.getLocale());
-        ctx.setVariable("currentDate", new Date());
-        WebConfig.createTemplateEngine(getServletContext()).
-                process("file-upload", ctx, response.getWriter());
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        File fileSaveDir = new File(this.uploadDir);
+        if (! fileSaveDir.exists()) {
+            throw new IllegalStateException("Upload dir does not exist: " + this.uploadDir);
+        }
+
+        //Do this only if you are sure there won't be any file name conflicts!
+        //An existing one will simply be overwritten
+        String fileName;
+        for (Part part : request.getParts()) {
+            fileName = part.getSubmittedFileName();
+            part.write(this.uploadDir + File.separator + fileName);
+        }
+
+        //go back to the upload form
+        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale());
+        ctx.setVariable("message", "upload successful, wanna do another on?");
+        ctx.setVariable("Upload_directory", uploadDir );
+        templateEngine.process("file-upload", ctx, response.getWriter());
     }
-
 }
-
-
