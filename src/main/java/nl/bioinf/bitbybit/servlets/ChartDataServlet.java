ecac9891476a5fb8d00cd5d2df29b4e347ee0745
Muniru <mawal-umaru@st.hanze.nl>
2024-01-15 11:54:51 +0100

    Progress: 15 jan 2024

---
+++ src/main/java/nl/bioinf/bitbybit/servlets/ChartDataServlet.java
@@ -0,0 +1,45 @@
+package nl.bioinf.bitbybit.servlets;
+
+import com.google.gson.Gson;
+import nl.bioinf.bitbybit.data.WatchData;
+import nl.bioinf.bitbybit.file.FitBitParser;
+import nl.bioinf.bitbybit.file.WatchParser;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
+import nl.bioinf.bitbybit.config.WebConfig;
+import org.thymeleaf.TemplateEngine;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+@WebServlet(name = "ChartDataServlet", urlPatterns = "/chartdata")
+public class ChartDataServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        WatchParser l = new FitBitParser();
+        WatchData data = l.Parse("/homes/mawalumaru/Documents/Themas/Thema_10/ThemaOpdracht10/data/");
+        System.out.println(data.stepData.size());
+
+
+        int subset = data.GetSteps(1702214640, 1702891680);
+        System.out.println("Steps: " + subset);
+
+        String json = new Gson().toJson(data);
+
+        // Return results as JSON
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
