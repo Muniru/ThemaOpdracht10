3962c352ada57fc016478df8d123a7f1ec02d461
lepannettreb <l.ten.napel@st.hanze.nl>
2024-01-16 13:50:08 +0100

    after i unzipped and rezipped the files this extractor works

--- src/main/java/nl/bioinf/bitbybit/file/TestZipReader.java
+++ src/main/java/nl/bioinf/bitbybit/file/TestZipReader.java
@@ -38,3 +38,50 @@ import java.util.zip.ZipInputStream;
 //        }
 //    }
 //}
+
+public class TestZipReader {
+    public static void main(final String[] args) throws IOException {
+        final String fileZip = "C:\\Users\\btnap\\OneDrive - Hanzehogeschool" +
+                " Groningen\\Documents\\School\\Thema_10\\ThemaOpdracht10\\testdata\\samsunghealth_btnapel_20231122112365.zip";
+        final File destDir = new File("C:\\Users\\btnap\\OneDrive - Hanzehogeschool Groningen" +
+                "\\Documents\\School\\Thema_10\\ThemaOpdracht10\\temp_test\\extracted");
+        final byte[] buffer = new byte[16384];
+        final ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            final File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                final FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+}
